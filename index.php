7c3d4908bb85edf2c091479e5d2bfb23a8fb3899
facundo <fzabaleta@pav-online.com>
2018-03-20 15:32:09 +0100

    commit6

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'hola don pepito';
+            echo 'hola don e pito';
         ?>
     </body>
 </html>
