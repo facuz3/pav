85b8c09f597777fb1a0352a3a7ae95f43efed7d9
facundo <fzabaleta@pav-online.com>
2018-03-20 15:52:48 +0100

    comrama4_2

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <?php
             echo 'adios don pepito';
             echo 'hola d        on jos';
-            echo 'ricolaaaaaa ';
+            echo 'ricolaaa  aaa ';
         ?>
     </body>
 </html>
